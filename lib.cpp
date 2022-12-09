bd0e8f32db45c78f5d48459e580e0d6e5a8c946f
AleTodaroCbr650r <115482662+AleTodaroCbr650r@users.noreply.github.com>
2022-12-09 11:17:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool carattere (char &a) {
+
+
+    if ((a > 64) && (a < 91)) {
+        a = a + 32;
+        return true;
+
+
+    } else if ((a > 96) && (a < 123)) {
+        a = a - 32;
+        return true;
+
+    } else {
+        return false;
+    }
+}
