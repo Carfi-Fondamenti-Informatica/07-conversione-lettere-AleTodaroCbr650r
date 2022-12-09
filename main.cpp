746897249611757b3b0885bf1a7c9119a2890a92
AleTodaroCbr650r <115482662+AleTodaroCbr650r@users.noreply.github.com>
2022-12-09 11:16:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+
 
 int main(){
+  char a=0;
+  cin >> a;
+  if(carattere(a)==1){
+        cout << char(a) << endl;
+    }
+
+    else{
+        cout << "errore" << endl;
+    }  
   return 0;
 }
